a82e22397c9cdf4e3e656b5582c86e32efbaffbb
EmanuelGSouza <emanuelgdesouza@gmail.com>
2018-04-01 09:39:40 -0300

    added logic and behavior on search cep page

--- src/pages/cep/search.vue
+++ src/pages/cep/search.vue
@@ -1,5 +1,62 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page>
+    <p class="caption"> Encontre um CEP a partir de um logradouro </p>
+
+    <div class="row sm-gutter">
+      <div class="col">
+        <q-select
+          filter
+          autofocus-filter
+          v-model="uf"
+          stack-label="Selecione um estado"
+          :options="statesSiglaOptions" />
+      </div>
+      <div class="col">
+        <q-select
+          filter
+          autofocus-filter
+          v-model="city"
+          stack-label="Selecione um municipio"
+          :options="cityOptions" />
+      </div>
+      <div class="col">
+        <q-field>
+          <q-input
+            stack-label="Digite um logradouro"
+            v-model="logradouro"
+            :disable="isDisable" />
+        </q-field>
+      </div>
+    </div>
+
+    <br />
+
+    <q-btn
+      icon="search"
+      label="Pesquisar"
+      color="primary"
+      :disable="!isValid"
+      @click="getApiInformations" />
+
+    <br />
+
+    <div class="row sm-gutter">
+      <div class="col">
+        <p class="caption"> Informações preenchidas </p>
+
+        <div class="q-pa-sm">
+          <vue-json-pretty :data="informations" />
+        </div>
+      </div>
+
+      <div class="col">
+        <p class="caption"> Resultado </p>
+
+        <div class="q-pa-sm">
+          <vue-json-pretty :data="result" />
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -7,7 +64,100 @@
 </style>
 
 <script>
+import { QField, QInput, QBtn, QSelect } from 'quasar'
+import VueJsonPretty from 'vue-json-pretty'
+import { mapGetters, mapMutations, mapState } from 'vuex'
+import { isEmpty } from 'lodash'
+import * as types from '../../store/mutation-types'
+import { cepApi, ibgeApi } from '../../support/http'
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: { QField, QInput, QBtn, QSelect, VueJsonPretty },
+  data: () => ({
+    uf: '',
+    logradouro: '',
+    city: '',
+    cities: [],
+    result: {}
+  }),
+  computed: {
+    ...mapState(['stateSelected']),
+    ...mapGetters(['statesSiglaOptions', 'hasStateSelected']),
+    informations () {
+      return {
+        uf: this.uf,
+        city: this.city,
+        logradouro: this.logradouro,
+        stateSelected: this.stateSelected,
+        hasStateSelected: this.hasStateSelected
+      }
+    },
+    isDisable () {
+      return isEmpty(this.uf) || isEmpty(this.city)
+    },
+    isValid () {
+      return !this.isDisable && !isEmpty(this.logradouro)
+    },
+    cityOptions () {
+      return this.cities.map(city => {
+        return {
+          value: city.nome,
+          label: city.nome
+        }
+      })
+    }
+  },
+  watch: {
+    uf (value) {
+      this.setState(value)
+    },
+    hasStateSelected (value) {
+      if (value) {
+        this.$q.loading.show()
+        return ibgeApi
+          .get(`/v1/localidades/estados/${this.stateSelected.id}/municipios`)
+          .then(result => {
+            this.cities = result.data || []
+          })
+          .catch(err => {
+            console.error(err)
+            this.$q.notify({
+              type: 'negative',
+              message: err.message
+            })
+          })
+          .then(() => {
+            this.$q.loading.hide()
+          })
+      }
+    }
+  },
+  methods: {
+    ...mapMutations({
+      setState: types.SELECTED_STATE
+    }),
+    getApiInformations () {
+      if (!this.isDisable) {
+        this.$q.loading.show()
+        const { logradouro, uf, city } = this
+        return cepApi
+          .get(`/${uf}/${city}/${logradouro}/json`)
+          .then(result => {
+            this.result = result.data || {}
+          })
+          .catch(err => {
+            console.error(err)
+            this.$q.notify({
+              type: 'negative',
+              message: err.message
+            })
+          })
+          .then(() => {
+            this.$q.loading.hide()
+          })
+      }
+    }
+  }
 }
 </script>
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,10 +1,13 @@
+import { find } from 'lodash'
 import * as types from './mutation-types'
 
 export default {
   [types.SET_STATES] (state, states) {
     state.states = states || []
   },
-  [types.SELECTED_STATE] (state, _state) {
+  [types.SELECTED_STATE] (state, uf) {
+    const states = state.states || []
+    const _state = find(states, _state => _state.sigla === uf)
     state.stateSelected = _state || {}
   }
 }
