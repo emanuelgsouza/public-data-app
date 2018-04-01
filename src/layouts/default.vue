64a4ae042d7da496b2b1354c57952e3bf4a46957
EmanuelGSouza <emanuelgdesouza@gmail.com>
2018-03-31 22:00:19 -0300

    code generated by quasar-cli

---
+++ src/layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-layout-header>
+      <q-toolbar
+        color="primary">
+        <q-toolbar-title>
+          Public Data App
+          <div slot="subtitle">Uma aplicação para consumo de dados públicos do IBGE</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'LayoutDefault',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
